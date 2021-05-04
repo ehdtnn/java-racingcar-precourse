e43667c558dfd7110c4580e1ac2068791fb368bd
ehdtnn <ehdtnn@gmail.com>
2021-05-04 21:58:21 +0900

    feat : CarName, CarPosition 구현

--- src/main/java/racingcar/CarName.java
+++ src/main/java/racingcar/CarName.java
@@ -1,4 +1,29 @@
 package racingcar;
 
 public class CarName {
+
+    public static final String EMPTY = "";
+    public static final int MAX_NAME_SIZE = 5;
+
+    private String name;
+
+    public CarName(String name) {
+        if (!validNameEmpty(name)) {
+            throw new IllegalArgumentException("자동차 이름은 공백 사용 금지");
+        }
+        if (!validNameSize(name)) {
+            throw new IllegalArgumentException("자동차 이름은 5자 초과 금지");
+        }
+        this.name = name;
+    }
+
+    private boolean validNameEmpty(String name) {
+        return !EMPTY.equals(name);
+    }
+
+    public static boolean validNameSize(String name) { return name.length() <= MAX_NAME_SIZE; }
+
+    public String getName() {
+        return this.name;
+    }
 }
--- src/main/java/racingcar/CarPosition.java
+++ src/main/java/racingcar/CarPosition.java
@@ -1,4 +1,31 @@
 package racingcar;
 
 public class CarPosition {
+
+    public static final int MIN_POSITION = 0;
+
+    private int position;
+
+    public CarPosition() {
+        this.position = getPosition();
+    }
+
+    public CarPosition(int position) {
+        if (validPosition()) {
+            throw new IllegalArgumentException("위치 값은 0보다 같거나 커야 합니다.");
+        }
+        this.position = position;
+    }
+
+    public boolean validPosition() {
+        return this.position >= MIN_POSITION;
+    }
+
+    public int addPosition() {
+        return ++position;
+    }
+
+    public int getPosition() {
+        return position;
+    }
 }
