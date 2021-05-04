420a3f188321f2cb166676f4d5c62d423800c486
ehdtnn <ehdtnn@gmail.com>
2021-05-04 22:10:57 +0900

    feat : Car 학습 및 테스트 코드 작성

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -1,4 +1,56 @@
 package racingcar;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 public class CarTest {
+    @Test
+    @DisplayName("자동차_생성_성공")
+    public void 자동차_생성_성공() {
+        Car car = new Car("car1");
+        assertThat(car).isNotNull();
+    }
+
+    @Test
+    @DisplayName("자동차_생성_이름_공백_실패")
+    public void 자동차_생성_이름_공백_실패() {
+        assertThatThrownBy(() -> {
+            new Car("");
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("공백");
+    }
+
+    @Test
+    @DisplayName("자동차_생성_이름_5자초과_실패")
+    public void 자동차_생성_이름_5자초과_실패() {
+        assertThatThrownBy(() -> {
+            new Car("abcdef");
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("5자 초과");
+    }
+
+    @Test
+    @DisplayName("자동차_생성_위치_초기값_0")
+    void 자동차_생성_위치_초기값_0() {
+        Car car = new Car("car1");
+        assertThat(car.getCarPosition()).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("자동차_전진_4부터_9까지")
+    void 자동차_전진() {
+        Car car = new Car("car1");
+        assertThat(car.move(4)).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("자동차_멈춤_0부터_3까지")
+    void 자동차_멈춤() {
+        Car car = new Car("car1");
+        assertThat(car.move(3)).isEqualTo(0);
+    }
 }
+
